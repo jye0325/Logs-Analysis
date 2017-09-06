f9245e175b2caa672a18fd420e3789c727238580
Jackie Ye <jye0325@live.com>
2017-09-05 21:58:30 -0400

    v1.0.0 Updated script to be fully functional

---
+++ README.md
@@ -0,0 +1,36 @@
+# Logs-Analysis
+
+The **Logs-Analysis** is a project written for **Udacity's Full Stack Developer Nanodegree Program**. 
+
+In this project, the `python` script will produce an output file of three different queries. 
+
+## Requirments
+
+Please ensure your system contains the following software prior to using this project:
+
+* Virtual Box
+* Custom Vagrant file from Udacity.com
+
+## Download
+
+To obtain a copy of this project download the entire contents of this repository in a `.zip` file onto your desktop or folder of choice and unzip it to your vagrant folder.
+
+## Usage
+
+1) Open terminal or command prompt and navigate to your Vagrant file.
+
+2) Enter `vagrant up` to initially set up your virtual machine.
+
+3) Once Vagrant has finished setting up, enter `vagrant ssh`. 
+
+3) Enter `python /vagrant/main.py` to run the script.
+
+4) The results of the query will be located in the same folder as `main.py` named `output.txt`.
+
+## Source
+
+## Disclaimer
+
+Please use this project at your own risk. I, _J. Ye._, am not responsible for any damage(s) that the end-user's computer may experience while using this project.
+
+## License
--- main.py
+++ main.py
@@ -5,94 +5,139 @@ import os
 
 DATABASE = "news"
 
-''' --TEST THIS OUT ~ USE SUBQUERIES
-CREATE TABLE sometable (t TIMESTAMP, d DATE);
-INSERT INTO sometable SELECT '2011/05/26 09:00:00';
-UPDATE sometable SET d = t; -- OK
--- UPDATE sometable SET d = t::date; OK
--- UPDATE sometable SET d = CAST (t AS date); OK
--- UPDATE sometable SET d = date(t); OK
-SELECT * FROM sometable ;
-'''
+# PSQL QUERY SETUP
+
+ERRORSETUP = '''
+    CREATE TABLE filterdate (t TIMESTAMP, d DATE);
+    INSERT INTO filterdate SELECT time FROM log;
+    UPDATE filterdate SET d = t;
+    
+    CREATE VIEW modifiedlog
+    AS SELECT d, status
+    FROM (SELECT *
+    FROM filterdate JOIN log
+    ON filterdate.t = log.time) AS results;
+    
+    CREATE TABLE ecount (ed date, errors integer);
+    INSERT INTO ecount (ed, errors) SELECT d, COUNT(status)
+    FROM modifiedlog WHERE (status <> '200 OK') GROUP BY d;
+    
+    CREATE TABLE tcount (td date, total integer);
+    INSERT INTO tcount (td, total) SELECT d, COUNT(status)
+    FROM modifiedlog GROUP BY d;
+    
+    CREATE TABLE pcount (pd date, percentage float);
+    INSERT INTO pcount (pd, percentage) SELECT ed, (errors*100/total)
+    FROM (SELECT * FROM ecount JOIN tcount ON ecount.ed = tcount.td) AS results;
+    '''
+
+VIEWPOPULARARTICLES = '''
+    SELECT articles.title, COUNT(log.path) AS NUMBEROFVIEWS FROM log
+    LEFT JOIN articles on articles.slug = split_part(log.path, '/', 3)
+    WHERE articles.title IS NOT NULL
+    GROUP BY title
+    ORDER BY NUMBEROFVIEWS DESC;
+    '''
+
+VIEWPOPULARAUTHORS = '''
+    SELECT authors.name, COUNT(log.path) AS NUMBEROFVIEWS FROM log
+    LEFT JOIN articles on articles.slug = split_part(log.path, '/', 3)
+    LEFT JOIN authors ON authors.id = articles.author
+    WHERE authors.name IS NOT NULL
+    GROUP BY name
+    ORDER BY NUMBEROFVIEWS DESC;
+    '''
 
 VIEWERRORS = '''
-CREATE TABLE filterdate (t TIMESTAMP, d DATE);
-INSERT INTO filterdate SELECT time FROM log;
-UPDATE filterdate SET d = t;
+    SELECT * FROM pcount WHERE percentage >= 1;
+    '''
 
-CREATE VIEW ERRORCOUNT
-AS SELECT method, status, d
-FROM (SELECT *
-FROM filterdate JOIN log 
-ON filterdate.t = log.time) as results
-WHERE (status <> '200 OK')
-GROUP BY d;
-'''
-
-SQLERRORS = '''
-SELECT (COUNT(log.status) AS NUMBEROFREQUESTS FROM LOGS /ERRORCOUNT.COUNT(status)) + (NUMBEROFREQUESTS%ERRORCOUNT.COUNT(status))
-'''
+DBCLEANUP = '''
+    DROP TABLE ecount, tcount, pcount;
+    DROP VIEW modifiedlog;
+    DROP TABLE filterdate;
+    '''
 
 
 def create_output():
+    """ This function stores the output of these three PSQL queries in a
+        .txt file called output. After performing the queries and writing the
+        file, the function will perform necessary cleanups before exiting the
+        program.
+        """
     content = ""
-    output_file = open('output.txt', 'w')
     content = popular_articles(content)
     content = popular_authors(content)
-    #http_errors(content)
+    content = http_errors(content)
     print(content)
+    output_file = open('output.txt', 'w')
     output_file.write(content)
     output_file.close()
+    database_cleanup()
 
 
 def popular_articles(content):
+    """ This function shall return the most popular articles of all time in
+        descending order.
+        """
     content = content
-    VIEWPOPULARARTICLES = '''
-    SELECT articles.title, COUNT(log.path) AS NUMBEROFVIEWS FROM log
-    LEFT JOIN articles on articles.slug = split_part(log.path, '/', 3)
-    WHERE articles.title is not null
-    GROUP BY title
-    ORDER BY NUMBEROFVIEWS DESC;
-	'''
     db = psycopg2.connect(database=DATABASE)
     c = db.cursor()
     c.execute(VIEWPOPULARARTICLES)
     results = c.fetchall()
-    test = content + "POPULAR ARTICLES OF ALL TIME\n" + "---------------------------------\n" + '\n'.join(str(e) for e in results) + "\n"
+    content = content \
+        + "POPULAR ARTICLES OF ALL TIME\n" \
+        + "---------------------------------\n" \
+        + '\n'.join(str(e) for e in results) \
+        + "\n"
     db.close()
-    return test
-
+    return content
 
 
 def popular_authors(content):
-	writecontent = content
-	VIEWPOPULARAUTHORS = '''
-	SELECT authors.name, COUNT(log.path) AS NUMBEROFVIEWS FROM log
-	LEFT JOIN articles on articles.slug = split_part(log.path, '/', 3)
-	LEFT JOIN authors ON authors.id = articles.author
-	WHERE authors.name is not null 
-	GROUP BY name
-	ORDER BY NUMBEROFVIEWS DESC;
-	'''
-	db = psycopg2.connect(database=DATABASE)
-	c = db.cursor()
-	c.execute(VIEWPOPULARAUTHORS)
-	results = c.fetchall()
-	test = writecontent + "\nPOPULAR AUTHORS OF ALL TIME\n" + "---------------------------------\n" + '\n'.join(str(e) for e in results) + "\n"
-	return test
-	db.close()
+    """ This function shall return the most popular authors of all time in
+        descending order.
+        """
+    content = content
+    db = psycopg2.connect(database=DATABASE)
+    c = db.cursor()
+    c.execute(VIEWPOPULARAUTHORS)
+    results = c.fetchall()
+    content = content \
+        + "\nPOPULAR AUTHORS OF ALL TIME\n" \
+        + "---------------------------------\n" \
+        + '\n'.join(str(e) for e in results) \
+        + "\n"
+    return content
+    db.close()
 
 
 def http_errors(content):
-	content = content
-	VIEWERRORS = '''
-	'''
-	db = psycopg2.connect(database=DATABASE)
-	c = db.cursor()
-	c.execute(VIEWERRORS)
-	results = c.fetchall()
-	writecontent = writecontent + "\nREPORTED ERRORS > 1\%\n" + "---------------------------------\n" + ''.join(str(e) for e in results) + "\n"
-	return content
-	db.close()
-	
+    """ This function shall return the days with errors greater than 1%.
+        """
+    content = content
+    db = psycopg2.connect(database=DATABASE)
+    c = db.cursor()
+    c.execute(ERRORSETUP)
+    c.execute(VIEWERRORS)
+    results = c.fetchall()
+    content = content \
+        + "\nREPORTED ERRORS > 1%\n" \
+        + "---------------------------------\n" \
+        + ''.join(str(e) for e in results) \
+        + "\n"
+    return content
+    db.close()
+
+
+def database_cleanup():
+    """This function shall cleanup the database by dropping all of the created
+        views and tables that were not originally created by newsdata.sql.
+        """
+    db = psycopg2.connect(database=DATABASE)
+    c = db.cursor()
+    c.execute(DBCLEANUP)
+    db.close()
+
+# Calls the main function at the start of the program
 create_output()
